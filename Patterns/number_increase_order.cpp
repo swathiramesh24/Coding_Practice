26000d21538ee8352c0c4ea39949d725a0e935a8
Swathi Ramesh <157286054+swathiramesh24@users.noreply.github.com>
2025-04-18 15:14:51 +0530

    Create number_increase_order.cpp

---
+++ Patterns/number_increase_order.cpp
@@ -0,0 +1,25 @@
+/*
+1
+1 2
+1 2 3
+1 2 3 4
+1 2 3 4 5
+
+*/
+
+//CODE:
+
+#include<iostream>
+
+int main()
+{
+    for(int i=1;i<=5;i++)
+    {
+        for(int j=1;j<=i;j++)
+        {
+            std::cout << j << " ";
+        }
+        std::cout << "\n";
+    }
+    return 0;
+}
